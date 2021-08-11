d449952cab1c765f3bc7beb6290fa30b5a6d96c6
CowFreedom <t@live.com>
2021-08-11 22:54:25 +0200

    Advection appears to work in a rudimentary way but more work required

--- examples/advection_scalar_quantity/main.cpp
+++ examples/advection_scalar_quantity/main.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-
+#include "..\..\src\gpu\hostgpu_bindings.h"
 
 template<class T>
 void print_scalar_field(T* A, int n, int m){
@@ -30,26 +30,40 @@ void run_example(T width, T height, int n, int m){
 	T* U=new T[2*n*m];
 	T* C=new T[n*m];
 	T t0=0.0;
-	T tend=0.25;
-	T dt=0.125;
+	T tend=5;
+	T dt=1;
 	for(int i=0;i<n*m;i++){
-		Q[i]=1;
+		Q[i]=0;
 		U[i*2]=1;
-		U[i*2+1]=2;
+		U[i*2+1]=1;
 	}
 	
-	T t=t0;
+	for (int i=0;i<n;i++){
+		for (int j=0;j<m;j++){
+			if (j>7){
+				//U[i*m*2+j*2]*=-1;
+			//	U[i*m*2+j*2+1]*=-1;
+				
+			}
+		}
+	}
+	
+	Q[m+5]=1;
+	Q[m+6]=3;
+	
+	T t=t0+dt;
 	std::cout<<"Q:\n";
 	print_scalar_field(Q,n,m);	
 	std::cout<<"\nU:\n";
 	print_vector_field(U,n,m);
 	while(t<=tend){
 		
-
-		
+		//Inefficient. Keep memory in device
+		gpu_advection_2d_f32(height, width,n,m, U,1,2*m, Q, 1,m, dt,C,1, m);
 		std::cout<<"\n"<<"t="<<t<<", Q:\n";
-		print_scalar_field(Q,n,m);		
-				
+	
+		std::memcpy(Q,C,sizeof(T)*n*m);
+		print_scalar_field(Q,n,m);	
 		t+=dt;
 	}
 	
@@ -59,9 +73,9 @@ void run_example(T width, T height, int n, int m){
 }
 
 int main(){
-	float width=1.0;
-	float height=1.0;
-	float x_points=32;
-	float y_points=32;
+	float width=18.0;
+	float height=18.0;
+	float x_points=18;
+	float y_points=18;
 	run_example(width,height,x_points,y_points);
 }
